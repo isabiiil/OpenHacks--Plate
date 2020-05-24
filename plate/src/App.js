e40888ead331370e20caa9753c012b2245c55848
Isabel Abonitalla <isabelabonitalla@gmail.com>
2020-05-23 20:45:20 -0400

    homepage with buttons

--- plate/src/App.js
+++ plate/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import Home from './home';
-// import Login from './login';
-// import Signup from './signup';
+import Login from './login';
+import Signup from './signup';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Home} />
-        {/* <Route path="/login" component={Login} />
-        <Route path="/register" component={Signup} /> */}
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Signup} />
         
       </Switch>
     </div>
--- plate/src/home.js
+++ plate/src/home.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+
+import 'antd/dist/antd.css';
 import './App.css';
+
 import HomeImage from './res/comp.svg';
 import LogoIcon from './res/dinner.svg';
 import Blob from './res/blob.svg';
-import { Link } from 'react-router-dom';
+
+import { Button } from 'antd';
 
 class Home extends Component {
     // constructor(props) {
@@ -15,7 +20,7 @@ class Home extends Component {
                 <div className = "header">
                     <Link to = "/">
                         <img className = "Logo" src = {LogoIcon} alt = "Logo" />
-                        <p>Plate.</p>
+                        <h6>Plate.</h6>
                     </Link>
                 </div>
                 <img className = "HomeImage" src = {HomeImage} alt = "Man next to project folders with check marks" />
@@ -23,6 +28,16 @@ class Home extends Component {
                 <div className = "description">
                     <h1>How much do you have on your PLATE?</h1>
                     <p>Keep track of all your tasks and responsibilities. Reprioritize based on time consumption and focus on efficiency instead. Get it done with Plate!</p>
+                    <Link to = "/login">
+                        <Button type="primary" shape="round">
+                            Log In
+                        </Button>
+                    </Link>
+                    <Link to = "/register">
+                        <Button type="primary" shape="round">
+                            Sign Up
+                        </Button>
+                    </Link>
                 </div>
             </div>
         );
