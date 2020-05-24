1022e836a8e8bf2a63a72c17b70d8da73e58be00
start-dash <53581091+start-dash@users.noreply.github.com>
2020-05-24 13:00:19 -0400

    tasha's src

---
+++ plate/src/src/containers/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+    return(
+        <div className="Home">
+            <div className="lander">
+                <h1>Plate</h1>
+            </div>
+        </div>
+    );
+}
